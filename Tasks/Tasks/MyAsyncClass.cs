524cd1b7c9fa501627a7dfc2500effeea4015282
WILDBERRIES\Zorin.Ivan <zorin.ivan@wildberries.ru>
2019-04-05 18:15:09 +0300

    доказательство

---
+++ Tasks/Tasks/MyAsyncClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class MyAsyncClass
+    {
+        public async Task SleepAsync()
+        {
+            for(int i=0; i<10;i++)
+            {
+                await Task.Delay(500);
+                Console.WriteLine($"{i * 500}");
+            }            
+        }
+    }
+}
---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Console.Read();
+        }
+
+        static async Task Test()
+        {
+            try
+            {
+                var cl = new MyAsyncClass();
+                Task t = cl.SleepAsync();
+                Console.WriteLine("Helloo");
+                Thread.Sleep(1000);
+                Console.WriteLine("world");
+                await t;
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.Read();
+            }
+        }
+    }
+}
